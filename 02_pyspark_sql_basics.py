9a3117285a8b340df89c139b4e3d315410bfcc16
CodeExplorer99 <157279320+CodeExplorer-99@users.noreply.github.com>
2026-06-01 20:04:32 +0530

    Update 02_pyspark_sql_basics.py

--- 02_pyspark_sql_basics.py
+++ 02_pyspark_sql_basics.py
@@ -1,1 +1,89 @@
 Day 2: Started PySpark SQL practice with Temp Views
+
+import sys
+import os
+
+# Set Python and Java paths
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+
+# ✅ Make sure this is a valid JDK path (not just JRE)
+os.environ['JAVA_HOME'] = r'C:\Users\ADMIN\.jdks\ms-17.0.15'
+
+# --------------------------------------------------------------------------------------------------
+
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+
+# Spark Configuration
+conf = SparkConf().setAppName("first") \
+    .setMaster("local[*]") \
+    .set("spark.default.parallelism", "1")
+
+# Initialize SparkContext and SparkSession
+sc = SparkContext(conf=conf)
+spark = SparkSession(sc)
+
+# --------------------------------------------------------------------------------------------------
+
+
+data = [
+    (0, "06-26-2011", 300.4, "Exercise", "GymnasticsPro", "cash"),
+    (1, "05-26-2011", 200.0, "Exercise Band", "Weightlifting", "credit"),
+    (2, "06-01-2011", 300.4, "Exercise", "Gymnastics Pro", "cash"),
+    (3, "06-05-2011", 100.0, "Gymnastics", "Rings", "credit"),
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "06-05-2011", 100.0, "Exercise", "Rings", "credit"),
+    (7, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (8, "02-14-2011", 200.0, "Gymnastics", None, "cash")
+]
+
+df = spark.createDataFrame(data, ["id", "tdate", "amount", "category", "product", "spendby"])
+df.show()
+
+
+
+
+
+data2 = [
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "02-14-2011", 200.0, "Winter", None, "cash"),
+    (7, "02-14-2011", 200.0, "Winter", None, "cash")
+]
+
+df1 = spark.createDataFrame(data2, ["id", "tdate", "amount", "category", "product", "spendby"])
+df1.show()
+
+
+
+
+data4 = [
+    (1, "raj"),
+    (2, "ravi"),
+    (3, "sai"),
+    (5, "rani")
+]
+
+
+
+cust = spark.createDataFrame(data4, ["id", "name"])
+cust.show()
+
+data3 = [
+    (1, "mouse"),
+    (3, "mobile"),
+    (7, "laptop")
+]
+
+prod = spark.createDataFrame(data3, ["id", "product"])
+prod.show()
+
+
+
+df.createOrReplaceTempView("df")
+df1.createOrReplaceTempView("df1")
+cust.createOrReplaceTempView("cust")
+prod.createOrReplaceTempView("prod")
+
